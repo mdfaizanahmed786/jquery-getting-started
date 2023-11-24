ef36ab4cc5de1743a9eed9685259c8fe5172ff04
Mohammed Faizan Ahmed <faizanzaid789@gmail.com>
2023-11-24 22:48:09 +0530

    feat: added some value changes

--- script.js
+++ script.js
@@ -68,18 +68,10 @@ $(".fadeOutDemo").click(function () {
 //   });
 $("selector").slideToggle("speed", "callback");
 
-
-
-
-
 // The jQuery stop() method is used to stop an animation or effect before it is finished.
 
 // The stop() method works for all jQuery effect functions, including sliding, fading and custom animations.
 // $(selector).stop(stopAll,goToEnd);
-
-
-
-
 
 // Importance of callback function:( it is optional)
 // $("button").click(function(){
@@ -88,53 +80,40 @@ $("selector").slideToggle("speed", "callback");
 //     });
 //   });
 
-
 // chaining is also possible:
 // $("#p1").css("color", "red").slideUp(2000).slideDown(2000);
 
-
-
-
-
-
-
-
 // Animate Now
-// Is it possible to manipulate ALL CSS properties with the animate() method?
+// Is it possible to manipulate ALL CSS properties with the anim  ate() method?
 
 // Yes, almost! However, there is one important thing to remember: all property names must be camel-cased when used with the animate() method: You will need to write paddingLeft instead of padding-left, marginRight instead of margin-right, and so on.
-$('.animate-btn').click(function(){
-  $('.animate-div').animate({left:"250px"})
-})
+$(".animate-btn").click(function () {
+  $(".animate-div").animate({ left: "250px" });
+});
 
-
-$(window).scroll(function() {
+$(window).scroll(function () {
   if ($(this).scrollTop() > 100) {
-    $('#scrollUpButton').fadeIn();
+    $("#scrollUpButton").fadeIn();
   } else {
-    $('#scrollUpButton').fadeOut();
+    $("#scrollUpButton").fadeOut();
   }
 });
 
 // Scroll up when the button is clicked
-$('#scrollUpButton').click(function() {
-  $('html, body').animate({scrollTop: 0}, 800);
+$("#scrollUpButton").click(function () {
+  $("html, body").animate({ scrollTop: 0 }, 800);
 });
 
+$(".content-btn").click(function () {
+  alert($(".get-content").text());
+  alert($(".get-content").html());
+  alert($(".get-content").attr("data-x"));
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+// The below code gives us value of input element...
+// $("#btn1").click(function(){
+//   alert("Value: " + $("#test").val());
+// });
 
 // there are many events as well..
 // 1. mousenter
